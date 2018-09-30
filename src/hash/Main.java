ac6b2ba1973094ff2d0ca3966b82d591a261745a
João Victor <victorvilela00@gmail.com>
2018-09-29 22:05:30 -0300

    main atualizado

--- src/hash/Main.java
+++ src/hash/Main.java
@@ -5,10 +5,13 @@ package hash;
  * @author victor
  */
 public class Main {
-
+    final static short X = 1;
+    final static short O = 2;
     public static void main(String[] args) {
         Game game = new Game();
-        game.startPvIA();
+        Player p1 = new HumanPlayer("Jogador Humano", X);
+        Player p2 = new MinimaxPlayer("Inteligência Artificial", O);
+        game.start(p1, p2);
     }
     
 }
